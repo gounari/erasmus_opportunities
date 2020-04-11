76b9419b802d07b994deaa0f9f3e77bd6272ea75
argyro <argyrogounari@gmail.com>
2020-04-11 21:16:20 +0100

    Add enhanced cards that flip

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -25,22 +25,102 @@ class _SignInState extends State<SignIn> {
           children: <Widget>[
 
             FlipCard(
-              front: Card(
-                elevation: 10.0,
-                color: Color.fromRGBO(231, 236, 239, 1.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width/2.2,
-                  height: MediaQuery.of(context).size.height,
-                  child: Image.asset(
-                    '/Users/argyrodevelop/AndroidStudioProjects/erasmus_opportunities/lib/assets/shapes1.jpg',
-                    fit: BoxFit.none,
-                    color: Colors.black,
-                    colorBlendMode: BlendMode.plus,
+              front: Stack(
+                children: <Widget>[
+                  Card(
+                    elevation: 10.0,
+                    color: Color.fromRGBO(231, 236, 239, 1.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/2.2,
+                          height: MediaQuery.of(context).size.height,
+                          child: Image.asset(
+                            '/Users/argyrodevelop/AndroidStudioProjects/erasmus_opportunities/lib/assets/onboarding4.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width/3,
+                              height: MediaQuery.of(context).size.height/5,
+                              child: FittedBox(
+                                fit: BoxFit.fill,
+                                child: Text(
+                                  'Welcome back!',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width/3,
+                              height: MediaQuery.of(context).size.height/5,
+                              child: FittedBox(
+                                fit: BoxFit.fill,
+                                child: Text(
+                                  'Time to post amazing \n opportunities.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width/2.2,
+                              height: MediaQuery.of(context).size.width/9,
+                            ),
+                            FlatButton(
+                              padding: EdgeInsets.fromLTRB(
+                                  MediaQuery.of(context).size.width/15,
+                                  0.0,
+                                  MediaQuery.of(context).size.width/15,
+                                  0.0),
+                              color: Colors.black54,
+                              splashColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.black,
+                                  width: 0.0,
+                                  style: BorderStyle.solid
+                                ),
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                              onPressed: () {},
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width/10,
+                                height: MediaQuery.of(context).size.height/10,
+                                child: FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text(
+                                    'SIGN UP',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                      ],
+                    ),
                   ),
-                ),
+                ],
               ),
               back: Container(
                 child: Text('Back'),
@@ -63,7 +143,7 @@ class _SignInState extends State<SignIn> {
                   width: MediaQuery.of(context).size.width/2.2,
                   height: MediaQuery.of(context).size.height,
                   child: Image.asset(
-                    '/Users/argyrodevelop/AndroidStudioProjects/erasmus_opportunities/lib/assets/shapes2.jpg',
+                    '/Users/argyrodevelop/AndroidStudioProjects/erasmus_opportunities/lib/assets/onboarding2.jpg',
                     fit: BoxFit.fill,
                   ),
                 ),
