a1fd15bdff707429623a716122929a60230af241
argyro <argyrogounari@gmail.com>
2020-04-11 11:50:12 +0100

    Add provider

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,15 @@
+import 'package:erasmusopportunities/models/user.dart';
 import 'package:erasmusopportunities/screens/authenticate/authenticate.dart';
 import 'package:erasmusopportunities/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+    print(user);
     // Return either Home or Authenticate widget
     return Authenticate();
   }
